0d2fb9e15f053f9c1b3b6536097a61fcb49e3a79
michellekunz <michelle.kunz@hsr.ch>
2019-11-27 18:59:58 +0100

    add trailing ,

--- superset/assets/src/geocoding/GeocodingForm.jsx
+++ superset/assets/src/geocoding/GeocodingForm.jsx
@@ -69,7 +69,7 @@ export class GeocodingForm extends React.Component {
     const { tables } = this.props;
     const tableArray = [];
     tables.forEach(table =>
-      tableArray.push({ label: table.fullName, value: table })
+      tableArray.push({ label: table.fullName, value: table }),
     );
     return tableArray;
   }
